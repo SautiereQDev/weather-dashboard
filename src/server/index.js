19a54cbb578cfb8b55683a957dc159814ba7c548
Quentin_Sautière <quentin.sautiere@etudian.univ-lr.fr>
2024-06-06 15:18:33 +0200

    setup expressjs

---
+++ src/server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import 'dotenv/config';
+import bodyParser from 'body-parser'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+app.use(bodyParser.json())
+
+app.use('/static', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
+});
