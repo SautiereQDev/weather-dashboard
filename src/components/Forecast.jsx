69a5b7b639f6b7c150794277c099eb0104a79a67
Quentin_Sautière <quentin.sautiere@etudian.univ-lr.fr>
2024-06-08 02:17:15 +0200

    improve readability

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import Card from "./Card.jsx";
 import Carousel from "./Carousel.jsx";
-import {LuSun, LuArrowUp, LuArrowDown } from "react-icons/lu";
+import {LuArrowDown, LuArrowUp, LuSun} from "react-icons/lu";
 
 const Forecast = () => {
     const [data, setData] = useState(null);
@@ -10,7 +10,7 @@ const Forecast = () => {
     useEffect(()=> {
         const abortController = new AbortController();
 
-        fetch(`http://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API_KEY}&q=Paris&days=10&aqi=no&alerts=no&lang=fr`)
+        fetch(`https://api.weatherapi.com/v1/forecast.json?key=${import.meta.env.VITE_WEATHER_API_KEY}&q=Paris&days=10&aqi=no&alerts=no&lang=fr`)
             .then(r => r.json())
             .then(data => setData(data))
             .catch(error => console.error(error))
@@ -33,13 +33,26 @@ const Forecast = () => {
                     <p className="text-xl ml-4">Aujourd'hui</p>
                     <p className="flex items-center gap-2 mr-4">
                         <LuSun size={26} className="mr-1"/>
-                        <span className="flex items-center"><LuArrowUp size={18}/><span className="font-bold mr-0.5">{data.forecast.forecastday[0].astro.sunrise.split(' ')[0]}</span>{data.forecast.forecastday[0].astro.sunrise.split(' ')[1]}</span>
-                        <span className="flex items-center"><LuArrowDown size={18}/><span className="font-bold mr-0.5">{data.forecast.forecastday[0].astro.sunset.split(' ')[0]}</span>{data.forecast.forecastday[0].astro.sunset.split(' ')[1]}</span>
+                        {/*Lever de soleil*/}
+                        <span className="flex items-center">
+                            <LuArrowUp size={18}/>
+                            <span
+                                className="font-bold mr-0.5">{data.forecast.forecastday[0].astro.sunrise.split(' ')[0]}
+                            </span>{data.forecast.forecastday[0].astro.sunrise.split(' ')[1]}
+                        </span>
+                        {/*Coucher de soleil*/}
+                        <span className="flex items-center">
+                            <LuArrowDown size={18}/>
+                            <span
+                                className="font-bold mr-0.5">{data.forecast.forecastday[0].astro.sunset.split(' ')[0]}</span>
+                            {data.forecast.forecastday[0].astro.sunset.split(' ')[1]}
+                        </span>
                     </p>
                 </div>
                 <hr className="mb-4 mt-1 border-gray-400"/>
                 <Carousel
-                    components={data.forecast.forecastday[0].hour.map((hour, index) => <Card data={hour} type={"hour"} key={index}/>)}
+                    components={data.forecast.forecastday[0].hour.map((hour) => <Card data={hour} type={"hour"}
+                                                                                      key={hour.time}/>)}
                     nbElements={8}/>
             </main>
 
