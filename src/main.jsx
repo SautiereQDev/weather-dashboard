e60d19d209274c65367f6725a6ce744e718fc45d
Quentin <sautiereq@gmail.com>
2024-08-07 01:18:40 +0200

    creating meteo cards

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {CityProvider} from "./contexts/CityContext.jsx";
-import {BrowserRouter} from "react-router-dom";
-import ErrorBoundary from "@/components/ErrorBoundary.jsx";
-import {WeatherProvider} from "@/contexts/WeatherContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
-		<ErrorBoundary>
-		  <CityProvider>
-			  <WeatherProvider>
-	        <App />
-			  </WeatherProvider>
-		  </CityProvider>
-		</ErrorBoundary>
-	</BrowserRouter>
-
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
