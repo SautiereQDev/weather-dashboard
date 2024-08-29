366e2544cc9f7c33f581b9d8c08dc6ab987825fd
Quentin_Sautière <quentin.sautiere@etudian.univ-lr.fr>
2024-08-29 10:23:55 +0200

    complete documentation for utils scripts

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,3 +1,10 @@
+/**
+ * Constructs a URL with query string parameters.
+ *
+ * @param {string} baseURI - The base URI of the URL.
+ * @param {Object} params - The parameters to be included in the query string.
+ * @returns {string} - The URL with query string parameters.
+ */
 export const setParams = (baseURI: string , params : Object) : string => {
   const queryString = Object.entries(params)
 	  .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
