8baee945c015a031c03662eab36818ce3baae813
Quentin_Sautière <quentin.sautiere@etudian.univ-lr.fr>
2024-08-30 13:06:06 +0200

    reformÃat project after rebase

--- src/components/ui/ThemeSwitch.jsx
+++ src/components/ui/ThemeSwitch.jsx
@@ -1,6 +1,6 @@
 import { LuMoon, LuSun  } from "react-icons/lu";
 import {useContext} from "react";
-import {ThemeContext} from "../App.jsx";
+import {ThemeContext} from "../../App.jsx";
 
 const ThemeSwitch = () => {
     const {theme, setTheme} = useContext(ThemeContext)
