8130bdbd93fce7bd0efad3274eab6c194777a33f
Quentin_Sautière <quentin.sautiere@etudian.univ-lr.fr>
2024-08-31 22:49:39 +0200

    add date on homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,6 +4,7 @@ import {useCityContext} from "@/contexts/CityContext.jsx";
 import NavBar from "@/components/NavBar.jsx";
 import useClosestCity from "@/hooks/useClosestCity.tsx";
 import DayCard from "@/components/DayCard.jsx";
+import {getCurrentDate} from "@/utils/date.ts";
 
 const HomePage = () => {
 	const {position} = useGeolocation();
@@ -21,8 +22,8 @@ const HomePage = () => {
 
 	return (<>
 			<NavBar/>
+			<h1 className="text-3xl font-bold text-center pt-5 pb-3">{getCurrentDate()}</h1>
 			{!city ? (<p className="text-md">loading...</p>) : <DayCard cityName={city} isCurrent={true}/>
-
 			}
 			<div className="flex gap-4 justify-evenly mt-6 mx-4">
 				{["Paris", "Lyon", "Nantes", "Tours", "Strasbourg", "Bordeaux", "Marseille"].map((city) => (
