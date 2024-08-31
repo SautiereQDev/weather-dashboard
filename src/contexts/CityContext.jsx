501a21143c1f2b436ec1a8ab2804d9fb4f67fd8c
Quentin Sautière <75904100+SautiereQDev@users.noreply.github.com>
2024-09-01 00:24:32 +0200

    Update CityContext.jsx

--- src/contexts/CityContext.jsx
+++ src/contexts/CityContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CityContext = createContext(null);
+export const CityContext = createContext(null);
 
 export const CityProvider = ({ children }) => {
   const [city, setCity] = useState('Paris');
