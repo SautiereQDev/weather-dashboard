d077e193f05927716b13754d86df17ae9879d816
Quentin_Sautière <quentin.sautiere@etudian.univ-lr.fr>
2024-09-01 12:23:31 +0200

    make new geolocation functional with sessionStorage

--- src/contexts/CityContext.jsx
+++ src/contexts/CityContext.jsx
@@ -1,10 +1,18 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CityContext = createContext(null);
 
 export const CityProvider = ({ children }) => {
-  const [city, setCity] = useState('Paris');
+  const [city, setCity] = useState(() => {
+    return sessionStorage.getItem('city') || 'Paris';
+  });
+
+  useEffect(() => {
+    if (city) {
+      sessionStorage.setItem('city', city);
+    }
+  }, [city]);
 
   return <CityContext.Provider value={{ city, setCity }}>{children}</CityContext.Provider>;
 };
